c32e3dfa1494c125877b29dbfcad7e324bcf893c
Diego <diegogplata@gmail.com>
2022-12-07 14:03:30 -0600

    introduce finished v1 of landing page for editor, footer bgColor and heroImage might be added

--- src/Pages/TemplateSelect.js
+++ src/Pages/TemplateSelect.js
@@ -112,47 +112,15 @@ export const TemplateSelect = () => {
                       
         )}
         </div> 
-        <footer className="footer">
-            <div className="container">
-                <div className="columns">
-                    <div className="column is-3 is-offset-2">
-                        <h2><strong>Category</strong></h2>
-                        <ul>
-                            {/* <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                            <li><a href="#">Vestibulum errato isse</a></li>
-                            <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                            <li><a href="#">Aisia caisia</a></li>
-                            <li><a href="#">Murphy's law</a></li>
-                            <li><a href="#">Flimsy Lavenrock</a></li>
-                            <li><a href="#">Maven Mousie Lavender</a></li> */}
-                        </ul>
-                    </div>
-                    <div className="column is-3">
-                        <h2><strong>Category</strong></h2>
-                        <ul>
-                            {/* <li><a href="#">Labore et dolore magna aliqua</a></li>
-                            <li><a href="#">Kanban airis sum eschelor</a></li>
-                            <li><a href="#">Modular modern free</a></li>
-                            <li><a href="#">The king of clubs</a></li>
-                            <li><a href="#">The Discovery Dissipation</a></li>
-                            <li><a href="#">Course Correction</a></li>
-                            <li><a href="#">Better Angels</a></li> */}
-                        </ul>
-                    </div>
-                    <div className="column is-4">
-                        <h2><strong>Category</strong></h2>
-                        <ul>
-                            {/* <li><a href="#">Objects in space</a></li>
-                            <li><a href="#">Playing cards with coyote</a></li>
-                            <li><a href="#">Goodbye Yellow Brick Road</a></li>
-                            <li><a href="#">The Garden of Forking Paths</a></li>
-                            <li><a href="#">Future Shock</a></li> */}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <script src="../js/bulma.js"></script>
-        </footer>
+        <footer class="footer has-text-centered">
+  <div class="content has-text-centered">
+    <p>
+      <strong>LITOGRAFIA GIL S.A. DE C.V</strong>. The source code is licensed
+      <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
+      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+    </p>
+  </div>
+</footer>
         </section>
 
     

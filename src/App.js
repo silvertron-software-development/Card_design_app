c6442c4a62bbb1346ee6e42ce4a7972f1e914061
Walter González-Plata Cortes <waltergpc@hotmail.com>
2023-01-09 22:27:35 -0600

    introduce new deps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import '@stripe/stripe-js'
 import { CardEditor } from './Pages/CardEditor'
 import { TemplateSelect } from './Pages/TemplateSelect'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'

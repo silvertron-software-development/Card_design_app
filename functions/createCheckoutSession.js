3f397bbacef866e57b321232dc97a971b8167247
Walter González-Plata Cortes <waltergpc@hotmail.com>
2023-01-22 19:57:40 -0600

    first stripe iteeration

---
+++ functions/createCheckoutSession.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+exports.handler = async function (event, context) {
+	const stripe = require('stripe')(process.env.REACT_APP_STRIPE_TEST_KEY)
+	const bodyJson = JSON.parse(event.body)
+	const { url, cartInfo: priceId } = bodyJson
+
+	const session = await stripe.checkout.sessions.create({
+		mode: 'payment',
+		line_items: [
+			{
+				price: priceId,
+				quantity: 1
+			}
+		],
+		metadata: {
+			url: url
+		},
+		success_url: 'https://waltergplata.com',
+		cancel_url: 'https://waltergplata.com'
+	})
+	console.log(session)
+	return {
+		statusCode: 200,
+		body: JSON.stringify(session.url)
+	}
+}
